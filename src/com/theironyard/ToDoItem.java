17e08815d7a1dcf83d711cc92215a007fb075a65
Erik <eschneider1128@gmail.com>
2016-05-23 11:17:51 -0400

    ToDo List

---
+++ src/com/theironyard/ToDoItem.java
@@ -0,0 +1,14 @@
+package com.theironyard;
+
+/**
+ * Created by Erik on 5/23/16.
+ */
+public class ToDoItem {
+    String text;
+    boolean isDone;
+
+    public ToDoItem(String text, boolean isDone) {  // Constructor
+        this.text = text;
+        this.isDone = isDone;
+    }
+}
